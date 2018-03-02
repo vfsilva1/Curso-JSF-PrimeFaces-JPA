1d9098992627a3ea0d8eef92f2bc6f716764710b
160098 <160098@A2918.labinfo.facens.br>
2018-03-01 22:32:33 -0300

    Inclusao da classe Professor

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,27 @@
+package sistema.modelos;
+
+public class Professor {
+	
+	private int cpf;
+	private String nome, curso;
+	
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+		
+}
